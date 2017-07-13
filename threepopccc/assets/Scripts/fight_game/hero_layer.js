d73cd9d2960b5fe5468b97f82ac24666acff6a8d
楚浩远 <328387458@qq.com>
2017-07-13 17:59:27 +0800

    none

--- threepopccc/assets/Scripts/fight_game/hero_layer.js
+++ threepopccc/assets/Scripts/fight_game/hero_layer.js
@@ -19,11 +19,6 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        // this.zIndex = -100;
-        // this.addStartDuraction = 0;
-        // this.bgList = [];
-        ///刚初始化进来，需要加载一些背景
-
 
         cc.loader.loadRes("shan.png",cc.SpriteFrame, (err, spriteFrame) => {
 
@@ -56,47 +51,8 @@ cc.Class({
         });
     },
 
-    addStart: function () {
-        // let data = global.gameDataController.getOneCellData();
-        // let start = cc.instantiate(this.StartPrefab);
-        // data.index = 0;
-        // start.getComponent("cell").init(data);
-        // start.parent = this.node;
-        // start.position = cc.p(this.camera.position.x + 100,0);
-    },
     update: function (dt) {
-        // this.heroNode.position = cc.p(this.heroNode.position.x + 1, this.heroNode.position.y);
-        // // this.camera.position = cc.p(this.heroNode.position.x + 200, this.camera.position.y);
-        // if (this.addStartDuraction > dt * 100){
-        //     this.addStartDuraction = 0;
-        //     this.addStart();
-        // }else {
-        //     this.addStartDuraction += dt;
-        // }
 
-        // if (this.bgList.length < 2){
-        //     let bg = cc.instantiate(this.bgUpPrefab);
-        //     bg.parent = this.bgUpLayer;
-        //
-        //     let index = this.bgList.length - 1;
-        //     if (index < 0){
-        //         bg.position = cc.p(0,0);
-        //     }else {
-        //         let lastBg = this.bgList[this.bgList.length - 1];
-        //         bg.position = cc.p(lastBg.position.x + lastBg.width,0);
-        //
-        //     }
-        //     cc.log("加一个背景 ");
-        //     this.bgList.push(bg);
-        // }
-        // for (let i = 0 ; i < this.bgList.length ; i ++){
-        //     if (this.camera.position.x - this.bgList[i].position.x > this.bgList[i].width){
-        //         this.bgUpLayer.removeChild(this.bgList[i]);
-        //         this.bgList.splice(i ,1);
-        //     }
-        // }
-
-        // this.starBgLayer.position = cc.p(this.starBgLayer.position.x - 1, this.starBgLayer.position.y);
 
 
     },
