7b56476834fe91c599bd34df7bc7cc214a8cb042
楚浩远 <328387458@qq.com>
2017-07-18 21:22:49 +0800

    tiledmap+ camera

--- threepopccc/assets/Scripts/main-world/game_map/game-map.js
+++ threepopccc/assets/Scripts/main-world/game_map/game-map.js
@@ -3,12 +3,30 @@ cc.Class({
 
     properties: {
 
+        playerNode : {
+            default: null,
+            type: cc.Node
+        },
+        camera: {
+            default: null,
+            type: cc.Node
+        }
     },
 
     // use this for initialization
     onLoad: function () {
         this.getComponent(cc.TiledMap);
     },
+    update: function (dt) {
+        this.playerNode.position = {
+            x: this.playerNode.position.x + 1,
+            y: this.playerNode.position.y
+        };
+        this.camera.position = {
+            x: this.playerNode.position.x,
+            y: this.playerNode.position.y - 500
+        };
+    }
 
 
 });
