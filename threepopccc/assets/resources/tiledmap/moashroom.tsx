09da7b89483bd3ee0a83dfc7a8ff2aed9d7b43a5
楚浩远 <328387458@qq.com>
2017-07-18 18:03:14 +0800

    瓦片地图

---
+++ threepopccc/assets/resources/tiledmap/moashroom.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="moashroom" tilewidth="80" tileheight="80" tilecount="1" columns="1">
+ <image source="../../../workspace/cocosCreatorTutorial/threepopres/切图/mash_room_0.png" width="80" height="80"/>
+ <terraintypes>
+  <terrain name="1" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+</tileset>
